c89f8bbc4984c98b57b67b59c0959c0ace3a0fe0
adamblufarb <adam.blufarb@gmail.com>
2026-04-22 21:58:50 +0300

    fix service worker: clear all caches, always fetch from network

--- docs/sw.js
+++ docs/sw.js
@@ -1,14 +1,14 @@
+const V = '1776884324';
+
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open('ima').then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
